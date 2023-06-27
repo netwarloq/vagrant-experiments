61146addcb1bb35bba69e6b66a0960e00c614178
NetWarloq <netwarloq@gmail.com>
2023-06-27 15:14:20 +0500

    add provision script

---
+++ js-fast-blog/js-fastify-blog/script.sh
@@ -0,0 +1,7 @@
+# Установка PostgreSQL
+sudo apt install postgresql
+
+# Установка Node.js
+curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash -
+sudo apt-get install gcc g++ make
+sudo apt-get install -y nodejs
